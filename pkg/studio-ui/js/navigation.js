c114107a37157f5e5fde7e0215eb37839f8096f7
Logan Allen <loganallc@gmail.com>
2021-10-25 17:45:32 -0500

    studio-ui: show which tab is selected in ui and also style mailing list buttons

--- pkg/studio-ui/js/navigation.js
+++ pkg/studio-ui/js/navigation.js
@@ -7,6 +7,11 @@ const landingPage = document.querySelector("#landing-page");
 const planetSalePage = document.querySelector("#planet-sale-page");
 const publishPage = document.querySelector("#publish-page");
 
+const buttons = {
+  planetSale: planetSaleBtn,
+  publish: publishBtn
+};
+
 const pages = {
   landing: landingPage,
   planetSale: planetSalePage,
@@ -17,9 +22,16 @@ const showPage = (key = null) => {
   if (!key) { return; }
   Object.keys(pages).forEach((k) => {
     if (k === key) {
+      if (buttons[k]) {
+        buttons[k].style = 'background-color: #EEEEEE;'
+      }
       pages[k].style = '';
     } else {
       pages[k].style = 'display: none;';
+
+      if (buttons[k]) {
+        buttons[k].style = '';
+      }
     }
   });
 };
