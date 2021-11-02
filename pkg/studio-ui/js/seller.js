0ad9030c397c8cdc9e7e5fcd0dd149277436919a
Logan Allen <loganallc@gmail.com>
2021-11-02 15:08:49 -0500

    studio-ui: added ability to add star config and remove it

--- pkg/studio-ui/js/seller.js
+++ pkg/studio-ui/js/seller.js
@@ -1,4 +1,49 @@
-//== Planet Sale Settings =====================================================
+//== Naive Market Settings ====================================================
+
+const addStarNameInp = document.querySelector("#add-star-name-input");
+const addStarPrvInp  = document.querySelector("#add-star-prv-input");
+const addStarProxyInp = document.querySelector("#add-star-proxy-input");
+
+const delStarInp = document.querySelector("#del-star-config-input");
+const setPlanetPriceInp = document.querySelector("#planet-price-input");
+const setReferralsInp = document.querySelector("#referral-input");
+const spawnShipsInp = document.querySelector("#spawn-ships-input");
+
+const addStarBtn = document.querySelector("#add-star-config-btn");
+const delStarBtn = document.querySelector("#del-star-config-btn");
+const setPlanetPriceBtn = document.querySelector("#planet-price-btn");
+const setReferralsBtn = document.querySelector("#referral-btn");
+const spawnShipsBtn = document.querySelector("#spawn-ships-btn");
+
+addStarBtn.onclick = () => {
+  naiveMarketPoke({
+    'add-star-config': {
+      ship: addStarNameInp.value,
+      config: {
+        prv: addStarPrvInp.value,
+        proxy: addStarProxyInp.value
+      }
+    }
+  });
+}
+
+delStarBtn.onclick = () => {
+  naiveMarketPoke();
+}
+
+setPlanetPriceBtn.onclick = () => {
+  naiveMarketPoke();
+}
+
+setReferralsBtn.onclick = () => {
+  naiveMarketPoke();
+}
+
+spawnShipsBtn.onclick = () => {
+  naiveMarketPoke();
+}
+
+//== Naive NMI Settings =====================================================
 
 const domainNameInp = document.querySelector("#domain-name-input");
 const saleAPIInp = document.querySelector("#sale-api-input");
