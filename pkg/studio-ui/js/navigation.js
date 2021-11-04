691909a5a0c8881f8b8858f09ecd883aa17a492c
Logan Allen <loganallc@gmail.com>
2021-11-04 16:19:47 -0500

    studio-ui: added inner navigation for sell page

--- pkg/studio-ui/js/navigation.js
+++ pkg/studio-ui/js/navigation.js
@@ -2,15 +2,25 @@
 
 const landingBtn = document.querySelector("#landing-btn");
 const publishBtn = document.querySelector("#publish-btn");
+
 const sellBtn = document.querySelector("#sell-btn");
+const sellStatusBtn = document.querySelector("#sell-status-btn");
+const sellSettingsBtn = document.querySelector("#sell-settings-btn");
+
 
 const landingPage = document.querySelector("#landing-page");
-const sellPage = document.querySelector("#sell-page");
 const publishPage = document.querySelector("#publish-page");
+
+const sellPage = document.querySelector("#sell-page");
+const sellStatusPage = document.querySelector("#sell-status-inner-page");
+const sellSettingsPage = document.querySelector("#sell-settings-inner-page");
+
 
 const buttons = {
   landing: landingBtn,
   sell: sellBtn,
+  sellStatus: sellStatusBtn,
+  sellSettings: sellSettingsBtn,
   publish: publishBtn
 };
 
@@ -38,12 +48,61 @@ const showPage = (key = null) => {
   });
 };
 
+
+const innerButtons = {
+  sell: {
+    status: sellStatusBtn,
+    settings: sellSettingsBtn
+  }
+};
+
+const innerPages = {
+  sell: {
+    status: sellStatusPage,
+    settings: sellSettingsPage
+  }
+};
+
+const showInnerPage = (outerKey = null, key = null) => {
+  if (!outerKey || !key) { return; }
+  Object.keys(innerPages[outerKey]).forEach((k) => {
+    if (k === key) {
+
+      if (innerButtons[outerKey][k]) {
+        innerButtons[outerKey][k].style = 'color: #000 !important;'
+      }
+
+      innerPages[outerKey][k].style = 'height: calc(100% - 2rem);';
+    } else {
+      innerPages[outerKey][k].style = 'display: none;';
+
+      if (innerButtons[outerKey][k]) {
+        innerButtons[outerKey][k].style = '';
+      }
+    }
+  });
+};
+
+
 landingBtn.onclick = (e) => { showPage('landing'); };
-sellBtn.onclick = (e) => { showPage('sell'); };
+
+sellBtn.onclick = (e) => { showPage('sell'); showInnerPage('sell', 'status'); };
+
+sellStatusBtn.onclick = (e) => {
+  showPage('sell');
+  showInnerPage('sell', 'status');
+};
+
+sellSettingsBtn.onclick = (e) => {
+  showPage('sell');
+  showInnerPage('sell', 'settings');
+};
+
 publishBtn.onclick = (e) => { showPage('publish'); };
 
 if (location.hash === '#sell') {
   showPage('sell');
+  showInnerPage('sell', 'status');
 } else if (location.hash === '#publish') {
   showPage('publish');
 } else {
